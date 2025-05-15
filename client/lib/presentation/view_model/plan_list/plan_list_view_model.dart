6045270334e494404aa4f5bdc8302724a61965a5
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2025-05-15 20:30:07 +0900

    Feat: Add PlanList Screen

--- client/lib/presentation/view_model/plan_list/plan_list_view_model.dart
+++ client/lib/presentation/view_model/plan_list/plan_list_view_model.dart
@@ -1,3 +1,54 @@
 import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class PlanListViewModel extends GetxController {}
+class PlanListViewModel extends GetxController {
+  final RxList<PlanSummary> plans = <PlanSummary>[].obs;
+
+  Future<void> fetchPlans(String userId) async {
+    final snapshot =
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userId)
+            .collection('plans')
+            .orderBy('createdAt', descending: true)
+            .get();
+
+    print ('Fetched ${snapshot.docs.length} plans for user: $userId');
+
+    plans.assignAll(
+      snapshot.docs.map((doc) => PlanSummary.fromJson(doc.data())),
+    );
+  }
+}
+
+class PlanSummary {
+  final String planId;
+  final String planName;
+  final String planThumbnailImage;
+  final String dialogId;
+  final int days;
+  final List<dynamic> mainSpots;
+  final DateTime createdAt;
+
+  PlanSummary({
+    required this.planId,
+    required this.planName,
+    required this.planThumbnailImage,
+    required this.dialogId,
+    required this.days,
+    required this.mainSpots,
+    required this.createdAt,
+  });
+
+  factory PlanSummary.fromJson(Map<String, dynamic> json) {
+    return PlanSummary(
+      planId: json['planId'],
+      planName: json['planName'],
+      planThumbnailImage: json['planThumbnailImage'],
+      dialogId: json['dialogId'] ?? '',
+      days: json['days'] ?? 0,
+      mainSpots: json['mainSpots'] ?? [],
+      createdAt: DateTime.parse(json['createdAt']),
+    );
+  }
+}
