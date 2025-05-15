06a83f80b275eea60b6f299fbb7d26262c6ba17a
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2025-05-15 20:44:06 +0900

    Feat: Add GeminiLiveTestPage for testing Live Chatbot functionality

--- client/lib/presentation/view_model/profile/profile_view_model.dart
+++ client/lib/presentation/view_model/profile/profile_view_model.dart
@@ -1,5 +1,5 @@
- 
- import 'package:get/get.dart';
+
+import 'package:get/get.dart';
 import 'package:tigo/app/utility/security_util.dart';
 import 'package:tigo/domain/entity/user_brief_state.dart';
 import 'package:tigo/domain/repository/user_repository.dart';
