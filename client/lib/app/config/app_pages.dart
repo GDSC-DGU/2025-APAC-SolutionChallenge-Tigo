fb7eb4455f4df864d5eae1d5c2d1df8de0eb9487
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2025-05-15 20:29:52 +0900

    Feat: Add PlanList Screen

--- client/lib/app/config/app_pages.dart
+++ client/lib/app/config/app_pages.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'package:tigo/app/config/app_routes.dart';
 import 'package:tigo/app/middleware/login_middleware.dart';
+import 'package:tigo/presentation/view/plan_list/plan_list_screen.dart';
 import 'package:tigo/presentation/view/tigo_plan_chat/tigo_plan_chat_screen.dart';
 import 'package:tigo/presentation/view/root/root_screen.dart';
 import 'package:tigo/presentation/view/sign_in/sign_in_screen.dart';
 import 'package:tigo/presentation/view/tigo_plan_completed_screen/tigo_plan_completed_screen.dart';
+import 'package:tigo/presentation/view_model/plan_list/plan_list_binding.dart';
 import 'package:tigo/presentation/view_model/tigo_plan_chat/tigo_plan_chat_binding.dart';
 import 'package:tigo/presentation/view_model/sign_in/sign_in_binding.dart';
 import 'package:tigo/presentation/view_model/tigo_plan_completed/tigo_plan_completed_binding.dart';
@@ -33,6 +35,11 @@ abstract class AppPages {
       name: AppRoutes.TIGO_PLAN_COMPLETED,
       page: () => const TigoPlanCompletedScreen(),
       binding: TigoPlanCompletedBinding(),
+    ),
+    GetPage(
+      name: AppRoutes.PLAN_LIST,
+      page: () => const PlanListScreen(),
+      binding: PlanListBinding(),
     ),
   ];
 }
